a62222f19d8b870a3ebffa3ced28773b0b06e6db
TeepuSultan1 <144908757+TeepuSultan1@users.noreply.github.com>
2023-09-21 23:15:32 -0700

    Add files via upload

---
+++ Goat.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+	cout << "     --------------------------------------" << endl;
+	cout << "" << endl; 
+	cout << "              o    ^ ___^" << endl;
+	cout << "                o   (oo)\\_______ " << endl;
+	cout << "" << endl;
+	cout << "                    (__)\\       )\\/\\" << endl;
+	cout << "                         ||---w |" << endl;
+	cout << "                         ||    ||" << endl;
+}
